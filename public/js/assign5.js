36af47a1b551b886bcf2dd438e655a325d3c2215
KewenGu <kewen.gu@gmail.com>
2015-12-07 21:18:40 -0500

    assignment 6

--- public/js/assign5.js
+++ public/js/assign5.js
@@ -47,7 +47,7 @@ function makeGet(url) {
 
   req.onreadystatechange = function() {
     handleRes(req);
-  };
+  }
 
   req.open('GET', url);
   req.send();
@@ -74,49 +74,3 @@ function buildList(A) {
     i++;
   });
 }
-
-
-var title = document.getElementById("title");
-
-title.addEventListener("mouseover", function(evt){
-	console.log("Mouse over title");
-	var el = document.getElementById("media");
-	el.innerHTML = '<div id="video-player" align="center" onclick="videoClick()"><p>Click the box to see a movie trailer</p></div>';
-});
-
-title.addEventListener("click", function(evt){
-	console.log("Clicked on title");
-	var el1 = document.getElementById("video-player");
-	el1.innerHTML = "<p>Click the Box to See</p>";
-	var el2 = document.getElementById("title");
-	el2.innerText = "Assignment 6";
-	el2.style.fontFamily = "'Lobster', cursive";
-	el2.style.fontSize = "5em";
-	el2.letterSpacing = "4px";
-});
-
-
-var header = document.getElementById("header");
-header.addEventListener("click", function(evt){
-	console.log("Clicked on header");
-	var el = document.getElementById("media");
-	el.innerHTML = "";
-});
-
-
-var media = document.getElementById('media');
-media.addEventListener("click", function(evt){
-	console.log("Clicked on media");
-	var el = document.getElementById("video-player");
-	el.innerHTML = '<iframe width="854" height="480" src="https://www.youtube.com/embed/yRUAzGQ3nSY" frameborder="0" allowfullscreen></iframe>';
-}, true);
-
-
-function videoClick(){
-	console.log("Clicked on video-player");
-	var el = document.getElementById("title");
-	el.innerText = "Inside Out - Official US Trailer";
-	el.style.fontFamily = '"Comic Sans MS", cursive, sans-serif';
-	el.style.fontSize = "3em";
-	el.letterSpacing = "0px";
-};
